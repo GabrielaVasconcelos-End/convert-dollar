2b5f739ea25706b04b265096581a3daaedeb6b5e
Gabriela Vasconcelos <gabriela0398@hotmail.com>
2026-05-08 19:35:04 -0300

    Obtendo a moeda selecionada

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+//obtendo os elementos do formulário.
+const form=document .querySelector('form');
 const amount=document .getElementById('amount');
+const currency=document .getElementById('currency');
 
-//Manipulando o input amount para receber somente números
+//Manipulando o input amount para receber somente números.
 amount.addEventListener("input",() => {
 
 const hasCharactersRegex = /\D+/g
 amount.value = amount.value.replace(hasCharactersRegex, '')
 
 })
+
+//Capturando o evento de submit(enviar) do formulário.
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+}
