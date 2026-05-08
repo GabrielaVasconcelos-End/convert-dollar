f7a0b716a681b808215c473027a3fd78f5959f40
Gabriela Vasconcelos <gabriela0398@hotmail.com>
2026-05-08 19:25:14 -0300

    Validando input

---
+++ script.js
@@ -0,0 +1,9 @@
+const amount=document .getElementById('amount');
+
+//Manipulando o input amount para receber somente números
+amount.addEventListener("input",() => {
+
+const hasCharactersRegex = /\D+/g
+amount.value = amount.value.replace(hasCharactersRegex, '')
+
+})
